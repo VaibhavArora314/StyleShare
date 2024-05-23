ffb31d5bd3f68fbd76b300a736d56c2a0f1f77ac
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-05-23 18:31:08 +0530

    increase snippet limit

--- backend/src/routes/post/zodSchema.ts
+++ backend/src/routes/post/zodSchema.ts
@@ -3,6 +3,6 @@ import zod from "zod";
 export const createPostSchema = zod.object({
     title: zod.string().min(3).max(30),
     description: zod.string().min(3).max(200),
-    codeSnippet: zod.string().min(3).max(1000),
+    codeSnippet: zod.string().min(3).max(5000),
     tags: zod.array(zod.string().min(2).max(20)).min(1).max(5),
 });
