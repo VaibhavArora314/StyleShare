bb8f52362eb23b52d28118ed84d76c981082ca52
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-05-23 18:18:58 +0530

    code reformating

--- frontend/src/pages/Post.tsx
+++ frontend/src/pages/Post.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import axios, { AxiosError } from 'axios';
 import { PostData } from '../types';
--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { PostData } from '../types';
