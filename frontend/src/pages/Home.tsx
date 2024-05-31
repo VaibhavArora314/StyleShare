99034a05db5b5e3d2e3d79d11231e813577a20ec
Rajveer <142779809+root-0101@users.noreply.github.com>
2024-05-31 11:51:45 +0000

    update home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-const Home = () => {
+function Home() {
   return (
     <div className="max-w-screen-xl mx-auto px-4 py-8 text-white">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8 items-center justify-center">
         <div>
           <h1 className="text-6xl font-bold mb-4">Welcome to Style Share</h1>
           <p className="text-xl  mb-6">
@@ -14,12 +14,13 @@ const Home = () => {
             <Link to="/app/posts">Explore</Link>
           </button>
         </div>
+        {/* <div className="h-58 w-45 bg-red"></div> */}
         <div className="hidden md:block">
-          {/* <img src={""} alt="Code" className="rounded-lg shadow-xl" /> */}
+          <img src={"https://img.freepik.com/free-vector/hand-drawn-web-developers_23-2148819604.jpg?w=996&t=st=1717153877~exp=1717154477~hmac=e1058df089e7d3f064f8e2a261e53f09f5aac845bf99095fcd7bf80767d31fa1"} alt="Code" className="rounded-lg shadow-lg shadow-blue-500/50 w-144" />
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Home;
