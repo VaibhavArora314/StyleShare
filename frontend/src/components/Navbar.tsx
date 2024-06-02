f2257df403b20015dd8311e8ceb8a13b4e12af75
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-06-02 22:00:17 +0530

    navbar alignment

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -94,19 +94,19 @@ const Navbar = () => {
               </div>
             ) : (
               <>
-                <li>
+                <li className="mt-2">
                   <Link to="/app/new-post" className={getNavLinkClass("/app/new-post")}>
                     New Post
                   </Link>
                 </li>
-                <li>
+                <li className="mt-2">
                   <Link to="/app/profile" className={getNavLinkClass("/app/profile")}>
                     Profile
                   </Link>
                 </li>
-                <li>
+                <li className="mt-2">
                   <button
-                    className="block py-2 px-3 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent"
+                    className="block py-2 px-3 rounded md:border-0 md:p-0 text-white md:hover:text-blue-500 hover:bg-gray-700 hover:text-white md:hover:bg-transparent w-full text-left"
                     onClick={handleLogout}
                   >
                     Logout
