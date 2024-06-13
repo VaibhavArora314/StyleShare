eb7fd3d50cac14ede95a9005cf5f475e4c28165a
Satuti@99 <satutiseth1566@gmauil.com>
2024-06-13 18:22:02 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -66,19 +66,20 @@ const Footer = () => {
                 </div>
             </div>
             <div className='block w-3/4 m-auto mt-10'>
-                    <div className='flex gap-4 justify-center md:justify-end'>
-                  <a href="https://github.com/VaibhavArora314/StyleShare" className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-shadow duration-300 ease">
-                   <img src={github} alt="github" className='pointer w-14 h-14' />
-                   </a>
-                 <Link to='/app' className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-transform duration-300 ease flex items-center justify-center p-2 text-white rounded-full focus:outline-none">
-                   <FaHome size={35} />
-                  </Link>
-                   </div>
-          </div>
-
-            <div className='mt-10 text-gray-300 xl:flex justify-center text-md lg:text-xl xl:text-md'>{t('footer.copy1')} {currentYear}{t('footer.copy2')}</div>
+                <div className='flex gap-4 justify-center md:justify-end'>
+                    <a href="https://github.com/VaibhavArora314/StyleShare" className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-shadow duration-300 ease">
+                        <img src={github} alt="github" className='pointer w-14 h-14' />
+                    </a>
+                    <Link to='/app' className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-transform duration-300 ease flex items-center justify-center p-2 text-white rounded-full focus:outline-none">
+                        <FaHome size={35} />
+                    </Link>
+                </div>
+            </div>
+            <div className='mt-10 text-gray-300 xl:flex justify-center text-md lg:text-xl xl:text-md'>
+                {t('footer.copy1')} {currentYear} {t('footer.copy2')}
+            </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
