0cb4c1686e66b99b9b52604d617a2b43d2f6b21f
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-13 23:03:31 +0530

    testimonials

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -26,6 +26,7 @@ import CustomizeWithAi from "./pages/CustomizeWithAi";
 import ScrollToTopWhenRouteChanges from "./components/ScrollToTopWhenRouteChanges";
 import './i18n';
 import LanguageDropdown from "./components/LanguageDropdown";
+import TestimonialSlider from "./components/TestimonialSlider";
 // import axios from "axios";
 // axios.defaults.baseURL = "http://localhost:3001/";
 
@@ -119,6 +120,7 @@ function App() {
               <Route path="*" element={<PageNotFound/>} />
             </Routes>
           </div>
+          <TestimonialSlider />
           <Footer />
         </React.Suspense>
       </RecoilRoot>
