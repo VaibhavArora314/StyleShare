ac6abd8baee0cff2b62cd643f3482b8ac94186a9
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-25 08:43:04 +0530

    Update Footer.tsx

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -17,11 +17,16 @@ const Footer = () => {
         <div className='bg-black text-white p-10'>
             <div className='md:flex'>
                 <div className='md:w-1/2'>
-                    <div className="flex items-center">
-                        <img src={logo} className="h-8" alt="Styleshare Logo" />
-                        <span className="ml-4 text-2xl font-semibold whitespace-nowrap text-white">
-                            {t('footer.heading')}
-                        </span>
+                    <div className="flex flex-col items-center md:items-start">
+                        <div className="flex items-center">
+                            <img src={logo} className="h-8" alt="Styleshare Logo" />
+                            <span className="ml-4 text-2xl font-semibold whitespace-nowrap text-white">
+                                {t('footer.heading')}
+                            </span>
+                        </div>
+                        <p className="mt-2 text-gray-300 text-sm md:text-base text-center md:text-left w-1/2">
+                            {t('A simple web-based platform where users can easily contribute, create, explore, share components.')}
+                        </p>
                     </div>
                 </div>
                 <div className='md:w-3/4 md:flex'>
@@ -70,17 +75,18 @@ const Footer = () => {
                 </div>
             </div>
            <div className='block w-3/4 m-auto mt-10'>
-                    <div className='flex gap-4 justify-center md:justify-end'>
-                  <a href="https://github.com/VaibhavArora314/StyleShare" className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-transform transition-shadow duration-300 ease">
-                   <img src={github} alt="github" className='pointer w-14 h-14' />
-                   </a>
-                 <Link to='/app' className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-transform transition-shadow duration-300 ease flex items-center justify-center p-2 text-white rounded-full focus:outline-none">
-                   <FaHome size={35} />
-                  </Link>
-                   </div>
-          </div>
-
-            <div className='mt-10 text-gray-300 xl:flex justify-center text-md lg:text-xl xl:text-md'>{t('footer.copy1')} {currentYear}{t('footer.copy2')}</div>
+                <div className='flex gap-4 justify-center md:justify-end'>
+                    <a href="https://github.com/VaibhavArora314/StyleShare" className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-transform transition-shadow duration-300 ease">
+                        <img src={github} alt="github" className='pointer w-14 h-14' />
+                    </a>
+                    <Link to='/app' className="hover:scale-110 hover:shadow-[0_0_10px_2px_blue] transition-transform transition-shadow duration-300 ease flex items-center justify-center p-2 text-white rounded-full focus:outline-none">
+                        <FaHome size={35} />
+                    </Link>
+                </div>
+           </div>
+           <div className='mt-10 text-gray-300 xl:flex justify-center text-md lg:text-xl xl:text-md'>
+                {t('footer.copy1')} {currentYear} {t('footer.copy2')}
+           </div>
         </div>
     )
 }
