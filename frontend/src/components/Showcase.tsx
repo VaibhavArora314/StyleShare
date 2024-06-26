3a288164843cecfb09d125f6ed6229f1d3ad218d
manikumarreddyu <133508962+manikumarreddyu@users.noreply.github.com>
2024-06-26 21:27:56 +0530

    Update Showcase.tsx

--- frontend/src/components/Showcase.tsx
+++ frontend/src/components/Showcase.tsx
@@ -20,13 +20,13 @@ const Showcase: React.FC = () => {
   return (
     <section className="text-[#000435] bg-white dark:text-white dark:bg-[#000435]">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <p className="text-2xl md:text-2xl sm:text-2xl lg:text-4xl xl:text-4xl 2xl:text-4xl  mb-6 lg:mb-14  font-extrabold text-center text-[#b752fa]  dark:text-white ">🚀  Our Code Editor 🚀</p>
+        <p className="text-2xl l lg:text-4xl xl:text-4xl 2xl:text-4xl  mb-6 lg:mb-14  font-extrabold text-center text-[#b752fa]  dark:text-white ">🚀  Our Code Editor 🚀</p>
         <div className="flex flex-col md:flex-row md:space-x-6 items-center">
           <div className="md:w-1/2 space-y-5">
             {features.map((feature, index) => (
               <div key={index} className="p-4 sm:p-4 md:p-6 lg:p-8 rounded-lg shadow-md border-2 border-grey-500 transition-transform duration-300 hover:-translate-y-2">
-                <h3 className="text-md sm:text-md md:text-xl lg:text-2xl font-bold mb-2 text-[#b752fa]  dark:text-white">{feature.title}</h3>
-                <p className="text-md sm:text-md md:text-md lg:text-lg font-mono">{feature.description}</p>
+                <h3 className="text-md  md:text-xl lg:text-2xl font-bold mb-2 text-[#b752fa]  dark:text-white">{feature.title}</h3>
+                <p className="text-md   lg:text-lg font-mono">{feature.description}</p>
               </div>
             ))}
           </div>
