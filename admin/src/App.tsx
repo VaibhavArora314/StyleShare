9cf39f764cea0faf30987965866455379859c816
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-06-30 15:08:25 +0530

    add backend dependencies

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,9 +1,15 @@
+import { BrowserRouter } from "react-router-dom"
+import { useRecoilValue } from "recoil"
+import { userState } from "./store/atoms/auth"
+
 function App() {
+  const user = useRecoilValue(userState);
+  console.log(user);
 
   return (
-    <>
+    <BrowserRouter>
       Admin Panel
-    </>
+    </BrowserRouter>
   )
 }
 
