c601f884d83a4834942f9ade80f73db1c6a087a6
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-06-30 15:11:23 +0530

    move recoil root and browser router to main

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
+  </React.StrictMode>
+);
