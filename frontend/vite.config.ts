7e711bc5bdb0e60503de68138842eaade21fa82f
naishub <patelnaisargi74@gmail.com>
2024-07-03 17:56:22 +0530

    final

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig,} from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: `/app`,
   plugins: [react()],
   define:{
-    'process.env.VITE_KEY':JSON.stringify(process.env.VITE_KEY)
+    'process.env.VITE_KEY':JSON.stringify(process.env.VITE_KEY),
+    'process.env.DOMAIN':JSON.stringify(process.env.DOMAIN),
+    'process.env.PROJECT_ID':JSON.stringify(process.env.PROJECT_ID),
+    'process.env.APP_ID':JSON.stringify(process.env.APP_ID)
   }
 })
