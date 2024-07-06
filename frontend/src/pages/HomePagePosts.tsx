2f1b9f1fd2a77b8f7d9e1714ef11e19378df4f95
VaibhavArora314 <vaibhav.afw@gmail.com>
2024-07-06 14:20:46 +0530

    posts ordering on home page

--- frontend/src/pages/HomePagePosts.tsx
+++ frontend/src/pages/HomePagePosts.tsx
@@ -31,7 +31,7 @@ const HomePagePost = () => {
         📃 {t("PostHeading")}
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full my-10">
-        {Array.from(posts).reverse().map((post, index) => (
+        {posts.map((post, index) => (
           <PostCard
             key={index}
             post={post}
