72203db1eedf41999d4c363f6a6e13184bf85c79
meet <dodiyameet041@gmail.com>
2024-07-08 23:18:34 +0530

    Added logout button on suspended page

--- frontend/src/pages/Blocked.tsx
+++ frontend/src/pages/Blocked.tsx
@@ -1,13 +1,22 @@
 import block from '../assets/block.png';
 import bg from '../assets/bgHero.png';
 import toast, { Toaster } from 'react-hot-toast';
+import { tokenState } from '../store/atoms/auth';
+import { useSetRecoilState } from 'recoil';
 
 const Blocked = () => {
+  const setTokenState = useSetRecoilState(tokenState);
 
   const handleCopy = () => {
     navigator.clipboard.writeText("admin@gmail.com");
     toast.success("Admin Email copied");
   }
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    setTokenState("");
+    toast.success('Logged out successfully');
+  };
   
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100" style={{ backgroundImage: `url(${bg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
@@ -22,11 +31,19 @@ const Blocked = () => {
         <p className="sm:text-lg font-semibold text-gray-700 mb-8 text-sm">
           If you believe this is a mistake, please contact the admin to resolve this issue.
         </p>
+        <div>
         <button onClick={handleCopy}
-          className="text-sm inline-block px-6 py-2 text-white bg-red-600 hover:bg-red-700 rounded-full font-semibold sm:text-xl"
+          className="text-sm inline-block px-6 py-2 mx-2 my-1 text-white bg-red-600 hover:bg-red-700 rounded-full font-semibold sm:text-xl"
         >
           Contact Admin
         </button>
+        <button 
+          onClick={handleLogout}
+          className="text-sm inline-block  px-6 py-2 mx-2 my-1 text-red-600 bg-transparent border-2 border-red-600 hover:bg-red-600 hover:text-white rounded-full font-semibold sm:text-xl transition duration-300"
+        >
+          Logout
+        </button>
+        </div>
       </div>
       <Toaster/>
     </div>
