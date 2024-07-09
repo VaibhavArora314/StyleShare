f9ba936726cbcdf14d19dfb4bef4a47ea348aefc
meet <dodiyameet041@gmail.com>
2024-07-10 02:37:37 +0530

    Admin managing posts added

--- backend/src/routes/admin/route.ts
+++ backend/src/routes/admin/route.ts
@@ -1,5 +1,5 @@
 import {Router} from 'express';
-import { adminLoginController, adminProfileController, allUserForAdmin, blockUserController, unblockUserController, getAdminPostsController, getAdminTrendingPostsController, getAdminStatsController } from './controller';
+import { adminLoginController, adminProfileController, allUserForAdmin, blockUserController, unblockUserController, getAdminPostsController, getAdminTrendingPostsController, getAdminStatsController, updatePostController, deletePostController, getPostByIdController } from './controller';
 import { isAdmin } from '../../middleware/adminAuth';
 
 const adminRouter = Router();
@@ -19,5 +19,11 @@ adminRouter.get("/posts/all", isAdmin,getAdminPostsController );
 adminRouter.get("/posts/trending", isAdmin,getAdminTrendingPostsController );
 
 adminRouter.get("/getCardStatus", isAdmin,getAdminStatsController );
+
+adminRouter.get('/postbyid/:id', isAdmin,getPostByIdController);
+
+adminRouter.patch('/posts/update/:postId', isAdmin, updatePostController);
+
+adminRouter.delete('/posts/delete/:postId', isAdmin, deletePostController);
 
 export default adminRouter;
