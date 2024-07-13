7d320b8c8ca5bb75db0ab80f1de8519762194d8f
meet <dodiyameet041@gmail.com>
2024-07-13 23:17:29 +0530

    Added syntax highlight in post page

--- frontend/src/components/PostCodeWindow.tsx
+++ frontend/src/components/PostCodeWindow.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import hljs from "highlight.js";
+import "highlight.js/styles/agate.css"; 
+
 function PostCodeWindow({
   codeSnippet,
   jsCodeSnippet,
@@ -7,26 +11,38 @@ function PostCodeWindow({
   codeSnippet: string;
   jsCodeSnippet: string;
   activeTab: string;
-  handleTabSwitch: (tab: "html" | "js") => void
+  handleTabSwitch: (tab: "html" | "js") => void;
 }) {
+  useEffect(() => {
+    hljs.highlightAll();
+  }, [codeSnippet, jsCodeSnippet, activeTab]);
+
+  useEffect(() => {
+    const highlightedCode = document.querySelectorAll("pre code");
+    highlightedCode.forEach((block) => {
+      block.removeAttribute("data-highlighted");
+      hljs.highlightElement(block as HTMLElement);
+    });
+  }, [activeTab]);
+
   return (
     <>
-      <div className="mb-2 ">
+      <div className="mb-2">
         <div className="flex p-1 border-2 border-sky-500 rounded">
           <button
             onClick={() => handleTabSwitch("html")}
-            className={`px-2 py-1  mx-1 font-semibold rounded-md ${
+            className={`px-2 py-1 mx-1 font-semibold rounded-md ${
               activeTab === "html"
-                ? "bg-sky-500 text-[#ffffff] dark:bg-sky-500 dark:text-white "
+                ? "bg-sky-500 text-[#ffffff] dark:bg-sky-500 dark:text-white"
                 : "border border-sky-500 text-sky-500 dark:text-white"
             }`}
           >
             HTML
           </button>
-          {jsCodeSnippet != "" ? (
+          {jsCodeSnippet !== "" && (
             <button
               onClick={() => handleTabSwitch("js")}
-              className={`px-2 py-1  mx-1 font-semibold rounded-md ${
+              className={`px-2 py-1 mx-1 font-semibold rounded-md ${
                 activeTab === "js"
                   ? "bg-sky-500 text-[#ffffff] dark:bg-sky-500 dark:text-white"
                   : "border border-sky-500 text-sky-500 dark:text-white"
@@ -34,19 +50,19 @@ function PostCodeWindow({
             >
               JavaScript
             </button>
-          ) : null}
-        </div>
-        <div className="">
-          {activeTab === "html" ? (
-            <pre className="p-4 text-[#000435] bg-sky-300 dark:text-white dark:bg-[#223bb9] border-4 border-sky-700 rounded overflow-auto max-h-96">
-              <code>{codeSnippet}</code>
-            </pre>
-          ) : (
-            <pre className="p-4 text-[#000435] bg-sky-300 dark:text-white dark:bg-[#223bb9] border-4 border-sky-700 rounded overflow-auto max-h-96">
-              <code>{jsCodeSnippet}</code>
-            </pre>
           )}
         </div>
+        <div className="">
+        {activeTab === "html" ? (
+          <pre className="p-4 text-[#000435] bg-sky-300 dark:text-white dark:bg-gray-800 border-4 border-sky-700 rounded overflow-auto max-h-96">
+            <code className="html rounded-lg">{codeSnippet}</code>
+          </pre>
+        ) : (
+          <pre className="p-4 text-[#000435] bg-sky-300 dark:text-white dark:bg-gray-800 border-4 border-sky-700 rounded overflow-auto max-h-96">
+            <code className="javascript rounded-lg">{jsCodeSnippet}</code>
+          </pre>
+        )}
+      </div>
       </div>
     </>
   );
