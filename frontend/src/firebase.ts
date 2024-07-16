96b2f4c9623d149c23a1f053f8ace31926224611
mr0nerd <adpcmecs1492@gmail.com>
2024-07-16 11:57:55 +0530

    Addressed PR review comments

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbCO0gIc0-0fXRNyE5IFtDQP1TveFoCko",
-  authDomain: "styleshare-18b7d.firebaseapp.com",
-  projectId: "styleshare-18b7d",
-  storageBucket: "styleshare-18b7d.appspot.com",
-  messagingSenderId: "219338980290",
-  appId: "1:219338980290:web:bcd0b62580593b4ba2345b",
-  measurementId: "G-YL8CTBCE71"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+
 };
 
 // Initialize Firebase
