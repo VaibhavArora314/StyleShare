5bd3974a76336f50deb57a030c7a288f00c0acb5
Bhavy Zala <126322584+zalabhavy@users.noreply.github.com>
2024-07-19 15:49:15 +0530

    Update About.tsx

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import bgHero from "../assets/bgHero.png";
 import Tilt from "react-parallax-tilt";
