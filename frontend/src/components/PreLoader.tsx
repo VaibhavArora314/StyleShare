5a67ecd4b8c78ebf7c2b5fb0b677c4cd163bc76a
meet <dodiyameet041@gmail.com>
2024-07-27 14:44:23 +0530

    Added preloader in style share

---
+++ frontend/src/components/PreLoader.tsx
@@ -0,0 +1,15 @@
+import "../styles/preloader.css";
+
+const Preloader = () => {
+  return (
+    <div className="preloader-container">
+      <svg viewBox="0 0 1320 300">
+        <text x="50%" y="50%" dy=".35em" textAnchor="middle">
+          Style Share
+        </text>
+      </svg>
+    </div>
+  );
+}
+
+export default Preloader;
