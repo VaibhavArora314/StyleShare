e15a84a6f8da616089afa3f0e6d54e96c4f4524e
meet <dodiyameet041@gmail.com>
2024-07-27 17:20:20 +0530

    Removed custom css and applied tailwind css

--- frontend/src/components/PreLoader.tsx
+++ frontend/src/components/PreLoader.tsx
@@ -1,11 +1,11 @@
-import "../styles/preloader.css";
+import "../index.css"
 
 const Preloader = () => {
   return (
-    <div className="preloader-container">
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center z-50">
       <svg viewBox="0 0 1320 300">
-        <text x="50%" y="50%" dy=".35em" textAnchor="middle">
-          Style Share
+        <text x="50%" y="50%" dy=".35em" textAnchor="middle" className="uppercase animate-stroke">
+          style share
         </text>
       </svg>
     </div>
