3b03682e3e82779e3f64d3d4f7979567c006b981
Utsavladia <utsavladia@gmail.com>
2024-08-10 10:25:14 +0530

    Fixed posts card for tablet size 768px

--- frontend/src/pages/Posts.tsx
+++ frontend/src/pages/Posts.tsx
@@ -186,7 +186,7 @@ const Posts = () => {
           <div className="text-center text-black dark:text-white">No Posts</div>
         ) : (
           <div>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 w-full">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full">
               {filteredPosts.map((post) => (
                 <PostCard
                   key={post.id}
